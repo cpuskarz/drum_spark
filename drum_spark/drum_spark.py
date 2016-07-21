bc2c6bf4f0f83505f89fe8f4f79a5ebe841fdf77
cpuskarz <cpuskarz@cisco.com>
2016-07-21 11:49:06 -0500

    no else

--- drum_spark/drum_spark.py
+++ drum_spark/drum_spark.py
@@ -144,8 +144,7 @@ def process_demoroom_message(post_data):
                 "To find out current status of voting, ask 'What are the results?'\n" \
                 "To find out the possible options, ask 'What are the options?\n" \
                 '''To place a vote, say "I'd like to vote" to start a private voting session.'''
-    else:
-        reply = "Enter something to do!"
+
 
     send_message_to_room(demo_room_id, reply)
 
